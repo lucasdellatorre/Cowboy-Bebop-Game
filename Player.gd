c3a9aac0877751f9541022d78dc1aa109504026e
Gustavo Vicentini Deon <gustavo.deon@edu.pucrs.br>
2023-05-08 22:05:39 -0300

    Finalizando movimentação do player

--- Player.gd
+++ Player.gd
@@ -1,10 +1,30 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 180
 
-onready var _animated_sprite = $AnimatedSprite
+onready var sprite = $AnimatedSprite
 
 var velocity = Vector2()
+
+func get_8way_input():
+	velocity.x = Input.get_action_strength("movement_right")-Input.get_action_strength("movement_left")	
+	velocity.y = Input.get_action_strength("movement_down")-Input.get_action_strength("movement_up")
+	velocity = velocity.normalized() * speed
+	if velocity.x > 0:
+		sprite.set_flip_h(false)
+		sprite.play("walk")
+	elif velocity.x < 0:
+		sprite.set_flip_h(true)
+		sprite.play("left")
+	# PRECISAMOS DE SPRITE DE INDO PRA BAIXO
+	#elif velocity.y > 0:
+	#	sprite.play("down")
+	elif velocity.y < 0:
+		sprite.play("walkup")
+	else:
+		sprite.play("walk")
+		sprite.stop()
+		sprite.frame = 0
 
 func get_input():
 	velocity = Vector2()
@@ -19,20 +39,9 @@ func get_input():
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-	get_input()
+	get_8way_input()
 	velocity = move_and_slide(velocity)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("movement_right"):
-		_animated_sprite.play("walk")
-		_animated_sprite.play("walk")
-	if Input.is_action_pressed("movement_up"):
-		_animated_sprite.play("walkup")
-	else:
-		_animated_sprite.stop()
